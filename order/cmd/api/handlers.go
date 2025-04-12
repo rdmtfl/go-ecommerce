db033f34e1572c43ba504213b77f8a8ea4750a63
Matheus <206574923+rdmtfl@users.noreply.github.com>
2025-04-12 15:55:23 -0300

    REFACTOR: remove struct declaration

--- order/cmd/api/handlers.go
+++ order/cmd/api/handlers.go
@@ -2,55 +2,24 @@ package main
 
 import (
 	"net/http"
-	"time"
 
 	"github.com/rdmtfl/order/data"
 )
 
-type RequestPayload struct {
-	OrderID      string    `json:"orderId"`
-	CustomerID   string    `json:"customerId"`
-	OrderDate    time.Time `json:"orderDate"`
-	Status       string    `json:"status"`
-	Subtotal     float64   `json:"subtotal"`
-	Tax          float64   `json:"tax"`
-	ShippingCost float64   `json:"shippingCost"`
-	Total        float64   `json:"total"`
-}
-
 // WriteLog -
 func (app *Config) WriteLog(w http.ResponseWriter, r *http.Request) {
 	// read json into var
-	var requestPayload RequestPayload
+	var requestPayload data.OrderEntry
 	_ = app.readJson(w, r, &requestPayload)
 
-	// insert data
-	event := data.OrderEntry{
-		OrderID:    requestPayload.OrderID,
-		CustomerID: requestPayload.CustomerID,
-		OrderDate:  requestPayload.OrderDate,
-		Status:     requestPayload.Status,
-		//Items:           requestPayload.Items,
-		Subtotal:     requestPayload.Subtotal,
-		Tax:          requestPayload.Tax,
-		ShippingCost: requestPayload.ShippingCost,
-		Total:        requestPayload.Total,
-		//ShippingMethod: requestPayload.ShippingMethod,
-		//PaymentMethod:  requestPayload.PaymentMethod,
-		//BillingAddress:  requestPayload.BillingAddress,
-		//ShippingAddress: requestPayload.ShippingAddress,
-		//Notes: requestPayload.Notes,
-		//Discounts:       requestPayload.Discounts,
-		//CreatedAt: requestPayload.CreatedAt,
-		//UpdatedAt: requestPayload.UpdatedAt,
-	}
-
-	err := app.Models.OrderEntry.Insert(event)
+	// send to db
+	err := app.Models.OrderEntry.Insert(requestPayload)
 	if err != nil {
 		app.errorJson(w, err)
 		return
 	}
 
+	// report user
 	resp := jsonResponse{
 		Error:   false,
 		Message: "logged",
