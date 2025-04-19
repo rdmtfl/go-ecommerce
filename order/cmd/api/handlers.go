772bd2c63ed08e8bd58d75dc6abccda5fe99a99b
Matheus <206574923+rdmtfl@users.noreply.github.com>
2025-04-19 11:34:48 -0300

    REFACTOR: remade response for broker

--- order/cmd/api/handlers.go
+++ order/cmd/api/handlers.go
@@ -2,28 +2,53 @@ package main
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/rdmtfl/order/data"
 )
 
+type responsePayload struct {
+	OrderId      string    `json:"orderId"`
+	CreationDate time.Time `json:"creationDate"`
+}
+
 // WriteLog -
 func (app *Config) WriteLog(w http.ResponseWriter, r *http.Request) {
-	// read json into var
 	var requestPayload data.OrderEntry
-	_ = app.readJson(w, r, &requestPayload)
+	var payload jsonResponse
+
+	// decode request
+	err := app.readJson(w, r, &requestPayload)
+	if err != nil {
+		app.errorJson(w, err, http.StatusBadRequest)
+		return
+	}
+
+	// validate payload
+	args := app.validatePayload(requestPayload)
+	if len(args) > 0 {
+		payload.Error = true
+		payload.Message = "missing fields"
+		payload.Data = args
+
+		app.writeJson(w, http.StatusBadRequest, payload)
+		return
+	}
 
 	// send to db
-	err := app.Models.OrderEntry.Insert(requestPayload)
+	err = app.Models.OrderEntry.Insert(requestPayload)
 	if err != nil {
 		app.errorJson(w, err)
 		return
 	}
 
-	// report user
-	resp := jsonResponse{
-		Error:   false,
-		Message: "logged",
+	// done correctly! report success
+	payload.Error = false
+	payload.Message = "created"
+	payload.Data = responsePayload{
+		OrderId:      requestPayload.OrderID,
+		CreationDate: time.Now(),
 	}
 
-	app.writeJson(w, http.StatusAccepted, resp)
+	app.writeJson(w, http.StatusAccepted, payload)
 }
