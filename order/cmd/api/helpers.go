217bba017e1ed711883debbc430436caaa6dbbf7
Matheus <206574923+rdmtfl@users.noreply.github.com>
2025-04-19 11:35:15 -0300

    FEAT: add order json validation

--- order/cmd/api/helpers.go
+++ order/cmd/api/helpers.go
@@ -3,8 +3,12 @@ package main
 import (
 	"encoding/json"
 	"errors"
+	"fmt"
 	"io"
 	"net/http"
+	"strings"
+
+	"github.com/rdmtfl/order/data"
 )
 
 type jsonResponse struct {
@@ -69,4 +73,91 @@ func (app *Config) errorJson(w http.ResponseWriter, err error, status ...int) er
 	payload.Message = err.Error()
 
 	return app.writeJson(w, statusCode, payload)
+}
+
+// validatePayload
+func (app *Config) validatePayload(payload data.OrderEntry) (args []string) {
+	args = make([]string, 0)
+
+	// order
+	if strings.EqualFold(payload.OrderID, "") {
+		args = append(args, "Order ID is required")
+	}
+	if strings.EqualFold(payload.CustomerID, "") {
+		args = append(args, "Customer ID is required")
+	}
+	if strings.EqualFold(payload.Status, "") {
+		args = append(args, "Status is required")
+	}
+	if payload.Subtotal <= 0 {
+		args = append(args, "Subtotal must be greater than 0")
+	}
+	if payload.Total <= 0 {
+		args = append(args, "Total must be greater than 0")
+	} else if payload.Total != payload.Subtotal+payload.Tax+payload.ShippingCost {
+		args = append(args, "Total must be equal to subtotal + tax + shipping cost")
+	}
+	if strings.EqualFold(payload.ShippingMethod, "") {
+		args = append(args, "Shipping method is required")
+	}
+	if strings.EqualFold(payload.PaymentMethod, "") {
+		args = append(args, "Payment method is required")
+	}
+
+	// // order items
+	if len(payload.Items) == 0 {
+		args = append(args, "At least one item is required")
+	} else {
+		for index, item := range payload.Items {
+			if strings.EqualFold(item.ProductID, "") {
+				args = append(args, fmt.Sprintf("Product ID is required for item %d", index))
+			}
+			if item.Quantity <= 0 {
+				args = append(args, fmt.Sprintf("Quantity must be greater than 0 for item %d", index))
+			}
+			if item.UnitPrice <= 0 {
+				args = append(args, fmt.Sprintf("Unit Price must be greater than 0 for item %d", index))
+
+			}
+			if strings.EqualFold(item.SKU, "") {
+				args = append(args, fmt.Sprintf("SKU is required for item %d", index))
+			}
+		}
+	}
+
+	// billing address
+	if strings.EqualFold(payload.BillingAddress.PostalCode, "") {
+		args = append(args, "Billing address postal code is required")
+	}
+	// shipping address
+	if strings.EqualFold(payload.ShippingAddress.PostalCode, "") {
+		args = append(args, "Shipping address postal code is required")
+	}
+
+	// order discounts
+	var sumOfDiscounts float64
+	if payload.Discounts != nil {
+		for index, discount := range payload.Discounts {
+			sumOfDiscounts += discount.Amount
+			if strings.EqualFold(discount.Code, "") {
+				args = append(args, fmt.Sprintf("Code required for discount %d", index))
+			}
+
+			if discount.Amount <= 0 {
+				args = append(args, fmt.Sprintf("Amount must be greater than 0 for discount %d", index))
+			}
+
+			if discount.IsPercent {
+				if discount.Amount > 100 {
+					args = append(args, fmt.Sprintf("Percentage cannot be greater than 100 for discount %d", index))
+				}
+			}
+		}
+
+		if sumOfDiscounts > payload.Subtotal {
+			args = append(args, "Sum of discounts cannot be greater than subtotal")
+		}
+	}
+
+	return args
 }
