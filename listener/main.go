3c60e8486385cc8e3e74851288d764e501a6463f
Matheus <206574923+rdmtfl@users.noreply.github.com>
2025-04-25 12:33:36 -0300

    CHORE: add conn

--- listener/main.go
+++ listener/main.go
@@ -1,11 +1,36 @@
 package main
 
+import (
+	"log"
+	"os"
+
+	amqp "github.com/rabbitmq/amqp091-go"
+)
+
 func main() {
 	// try to connect to rabbitmq
+	rabbitConn, err := connectRabbitMQ()
+	if err != nil {
+		log.Println("Failed to connect to RabbitMQ: ", err)
+		os.Exit(1)
+	}
+	defer rabbitConn.Close()
+
+	log.Println("Connected to RabbitMQ!")
 
 	// start listening for messages
 
 	// create consumer
 
 	// watch the queue and consume events
+}
+
+// connectRabbitMQ
+func connectRabbitMQ() (*amqp.Connection, error) {
+	conn, err := amqp.Dial("amqp://guest:guest@localhost")
+	if err != nil {
+		return nil, err
+	}
+
+	return conn, nil
 }
